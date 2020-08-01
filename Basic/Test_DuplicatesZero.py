e9d9ddfde1343d0d88fde754ab516a0a82295ca4
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-08-01 23:19:36 +0700

    Uploaded

---
+++ Basic/Test_DuplicatesZero.py
@@ -0,0 +1,15 @@
+def Solution(arr=list):
+    skip = 0
+    while skip < len(arr):
+        if arr[skip] == 0:
+            arr.insert(skip+1, 0)
+            arr.pop()
+            skip += 2
+        else:
+            skip += 1
+
+
+print(Solution([1, 0, 2, 3, 0, 4, 5, 0]))
+print(Solution([1, 2, 3]))
+print(Solution([1, 0]))
+print(Solution([0]*10000))
