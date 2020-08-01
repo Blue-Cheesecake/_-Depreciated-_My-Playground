6834fd0b03cf4fa502cbbedfdab9bfe68e8bffe1
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-08-01 13:57:04 +0700

    Changed NOT to Done

--- Basic/NOT_Test_ClimbingStair.py
+++ Basic/NOT_Test_ClimbingStair.py
@@ -1,20 +1,28 @@
 # https://leetcode.com/problems/climbing-stairs/
 
-import itertools
+# odd number +1, even number + 2
+# n! / n1!n2!
 
-# TLE And memory error
+
+def factorial(n):
+    r = 1
+    for i in range(1, n+1):
+        r *= i
+    return r
 
 
 def Solution(n=int):
     ini = [1] * (n - 2)
     ini.append(2)
-    # collapse 1 1 >> 2
-    count = 0
-    # even number is correct
+    result = 1
+    setting = 1
     if n % 2 == 0:
-        count += 2
-        while ini.index(2) >= 2:
-            count += len(set(list(itertools.permutations(ini))))
+        result = 2
+        setting = 2
+    try:
+        while ini.index(2) >= setting:
+            result += (factorial(len(ini)) //
+                       (factorial(ini.count(1))*factorial(ini.count(2))))
             cooldown = 0
             while 2 in ini:
                 ini.remove(2)
@@ -24,45 +32,30 @@ def Solution(n=int):
             while cooldown > -1:
                 ini.append(2)
                 cooldown -= 1
-    else:
-        count += 1
-        while ini.index(2) != 1:
-            count += len(set(list(itertools.permutations(ini))))
-            cooldown = 0
-            while 2 in ini:
-                ini.remove(2)
-                cooldown += 1
-            ini.pop()
-            ini.pop()
-            while cooldown > -1:
-                ini.append(2)
-                cooldown -= 1
-
-    return count
-
-# odd number +1, even number + 2
+    except:
+        pass
+    return result
 
 
-print(Solution(5))
-print(Solution(6))
-print(Solution(7))
-print(Solution(8))
-print(Solution(12))
-a = set(list(itertools.permutations([2, 1, 1, 1])))
-b = set(list(itertools.permutations([2, 2, 1])))
-print(a)
-print(b)
+for i in range(1, 46):
+    print(Solution(i))
 
-# n = 6
-# [1,1,1,1,1,1]
-c = set(list(itertools.permutations([1, 1, 1, 1, 2])))
-d = set(list(itertools.permutations([2, 2, 1, 1])))
+# print(factorial(4) / (factorial(3)))
+# print(factorial(3) / factorial(2))
+# a = set(list(itertools.permutations([2, 1, 1, 1])))
+# b = set(list(itertools.permutations([2, 2, 1])))
+# print(len(a) + len(b) + 1)
 
-print(len(c) + len(d) + 2)
+# # n = 6
+# # [1,1,1,1,1,1]
+# c = set(list(itertools.permutations([1, 1, 1, 1, 2])))
+# d = set(list(itertools.permutations([2, 2, 1, 1])))
 
-# n = 7
-e = set(list(itertools.permutations([2, 1, 1, 1, 1, 1])))
-f = set(list(itertools.permutations([2, 2, 1, 1, 1])))
-g = set(list(itertools.permutations([2, 2, 2, 1])))
+# print(len(c) + len(d) + 2)
 
-print(len(e)+len(g)+len(f)+1)
+# # n = 7
+# e = set(list(itertools.permutations([2, 1, 1, 1, 1, 1])))
+# f = set(list(itertools.permutations([2, 2, 1, 1, 1])))
+# g = set(list(itertools.permutations([2, 2, 2, 1])))
+
+# print(len(e)+len(g)+len(f)+1)
