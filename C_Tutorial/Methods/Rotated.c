11e57c0a19c7dbb11cfc36976b6bc49240dfd337
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-08-20 13:50:42 +0700

    Basic

---
+++ C_Tutorial/Methods/Rotated.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+void RoratedLeft(int *array[MAX_SIZE], int size)
+{
+    int temp = array[0];
+    for (int i = 0; i < size - 1; i++)
+    {
+        array[i] = array[i + 1];
+    }
+    array[size - 1] = temp;
+}
+// in the other hand, Rotate to the right is the same concept
+
+void RotatedRight(int *array[MAX_SIZE], int size)
+{
+    int temp = array[size - 1];
+    for (int i = size - 1; i > 0; i--)
+    {
+        array[i] = array[i - 1];
+    }
+    array[0] = temp;
+}
+
+int main()
+{
+    // expect 2 3 4 5 1
+    int list[5] = {1, 2, 3, 4, 5};
+    RoratedLeft(list, 5);
+    for (int j = 0; j < 5; j++)
+    {
+        printf("%d ", list[j]);
+    }
+    printf("\n");
+    // expect 5 1 2 3 4
+    int listA[5] = {1, 2, 3, 4, 5};
+    RotatedRight(listA, 5);
+    for (int k = 0; k < 5; k++)
+    {
+        printf("%d ", listA[k]);
+    }
+    return 0;
+}
