59fee0a76106b790ef75d6afb805133bcb2f7fd7
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-08-20 21:08:48 +0700

    Time Limit Exceed

--- Intermediete/Test_DiagonalTraverseII.py
+++ Intermediete/Test_DiagonalTraverseII.py
@@ -1,56 +1,38 @@
 # https://leetcode.com/problems/diagonal-traverse-ii/
 
-
+# Time Limit Exceed
+# This code must be able to support a lot of input (10 ^ 6)
 def Solution(nums=list):
+    if len(nums) == 1:
+        return nums[0]
     output = []
-    if len(nums) > 1:
-        # check if it square or rectangle
-        if len(nums[0]) == len(nums[len(nums) - 1]):
-            # index = index of row
-            # i = index of row (for adding)
-            # j = index of column (for adding)
-            # k = time to do
-            # count do untill reach k
-            # this process is done only half way
-            index = 0
-            while index < max(len(nums[0]), len(nums[len(nums) - 1])):
-                count = 0
-                i = index + 0
-                j = 0
-                # k depends on max len num[0], len num[index-1]
-                k = index + 1
-                while count < k:
-                    try:
-                        output.append(nums[i][j])
-                    except IndexError:
-                        pass
-                    count += 1
-                    i -= 1
-                    j += 1
-                index += 1
-            # the other half
-            # index for static last row (index + 1 if done) (hold j)
-            # i index of row (for adding, i -= 1)
-            # j index of column (for adding j += 1)
-            index = 1
-            while index != len(nums[len(nums) - 1]):
-                count = 0
-                i = len(nums) - 1
-                j = index + 0
-                k = k - 1
-                while count < k:
-                    try:
-                        output.append(nums[i][j])
-                    except IndexError:
-                        pass
-                    count += 1
-                    i -= 1
-                    j += 1
-                index += 1
+    MaxColumn = 0
+    for i in nums:
+        MaxColumn = max(len(i), MaxColumn)
+    # Time to do is from len(nums) + MaxColumn - 1
+    TimeTodo = len(nums) + MaxColumn - 1
+    count = 0
+    # do untill holdI == len(nums)
+    # if holdI == len(nums): holdJ += 1
+    holdI = 0
+    holdJ = 0
+    while count < TimeTodo:
+        i = holdI + 0
+        j = holdJ + 0
+        # condition to make i and j not exceed the frame
+        while j != MaxColumn and i >= 0:
+            try:
+                output.append(nums[i][j])
+            except IndexError:
+                pass
+            i -= 1
+            j += 1
+        count += 1
+        if holdI < len(nums) - 1:
+            holdI += 1
         else:
-            return 'Rectangle'
-    else:
-        output.append(nums[0][0])
+            holdJ += 1
+
     return output
 
 
@@ -64,8 +46,9 @@ print(Solution(nums=[[1, 2, 3], [4], [5, 6, 7], [8], [9, 10, 11]]))
 print(Solution(nums=[[1, 2, 3], [4]]))
 # # [1,4,2,3]
 print(Solution(nums=[[8, 2, 3, 4, 5]]))
-# # [8]
+# [8,2,3,4,5]
 print(Solution(nums=[[1, 2], [3, 4, 5, 6]]))
 # [1,3,2,4,5,6]
 print(Solution(nums=[[3, 4, 5, 6], [1, 2]]))
 # [3,1,4,2,5,6]
+print(Solution(nums=[[1, 2, 3, 4, 5, 6, 8] * 100000]))
