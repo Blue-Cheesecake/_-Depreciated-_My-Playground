674f57853f40c498dbf0dc8881b0792017544393
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-08-20 23:23:20 +0700

    Finished

---
+++ Basic/Test_MaxConsecutive.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int max(int num1, int num2)
+{
+    int result;
+    if (num1 > num2)
+    {
+        result = num1;
+    }
+    else
+    {
+        result = num2;
+    }
+    return result;
+}
+
+int Solution(int nums[], int size)
+{
+    int Maximum = 0;
+    int i = 0;
+    while (i < size)
+    {
+        int plus = 0;
+        int count = 0;
+        if (nums[i] == 1)
+        {
+            count += 1;
+            for (int j = i + 1; j < size; j++)
+            {
+                if (nums[j] == 1)
+                {
+                    plus += 1;
+                    count += 1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        Maximum = max(Maximum, count);
+        if (plus > 0)
+        {
+            i += plus;
+        }
+        else
+        {
+            i += 1;
+        }
+    }
+    return Maximum;
+}
+
+int main()
+{
+    int list[6] = {1, 1, 0, 1, 1, 1};
+    int listA[10] = {1, 0, 1, 1, 1, 1, 0, 1, 1, 1};
+    // 3
+    printf("%d\n", Solution(list, 6));
+    // 4
+    printf("%d\n", Solution(listA, 10));
+    return 0;
+}
---
+++ Basic/Test_SquaresOfArray.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void Sort(int array[], int n)
+{
+    int tmp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (array[j] < array[i])
+            {
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+    }
+}
+
+void Solution(int A[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = A[i] * A[i];
+    }
+    Sort(A, n);
+}
+
+int main()
+{
+    int list[5] = {-4, -1, 0, 3, 10};
+    // 0 1 9 16 100
+    Solution(list, 5);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", list[i]);
+    }
+
+    return 0;
+}
