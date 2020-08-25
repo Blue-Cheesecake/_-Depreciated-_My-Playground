656c771133520260b9ae36282c709c119a1de3fd
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-08-25 19:54:24 +0700

    Basic In-place

---
+++ Basic/Test_ReplaceElementswithGreatestElementonRightSide.py
@@ -0,0 +1,12 @@
+def Solution(arr=list):
+    maxSoFar = arr[len(arr) - 1]
+    arr[len(arr) - 1] = -1
+    # for backward
+    for i in range(len(arr) - 2, -1, -1):
+        temp = arr[i]
+        arr[i] = maxSoFar
+        maxSoFar = max(maxSoFar, temp)
+    return arr
+
+
+print(Solution(arr=[17, 18, 5, 4, 6, 1]))
