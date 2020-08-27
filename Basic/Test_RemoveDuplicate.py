68e09b0e89d8c98e949a92c8f96fca5faaa86768
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-08-27 12:00:35 +0700

    Added more Solution

--- Basic/Test_RemoveDuplicate.py
+++ Basic/Test_RemoveDuplicate.py
@@ -10,5 +10,27 @@ def Solution(nums=list):
             nums.remove(i)
     return len(nums)
 
+# Solution 2 is not in-place but compute only numbers of uniques
+
+
+def Solution2(nums=list):
+    if len(nums) == 0:
+        return 0
+    elif len(nums) == 1:
+        return nums[0]
+    out = 1
+    i = 1
+    ini = nums[0]
+    while i < len(nums):
+        if nums[i] == ini:
+            i += 1
+        else:
+            out += 1
+            ini = nums[i]
+            i += 1
+    return out
+
 
 print(Solution([1, 1, 1, 1]))
+print(Solution2(nums=[1, 1, 2, 2]))
+print(Solution2(nums=[0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
