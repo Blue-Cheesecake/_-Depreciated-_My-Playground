0c0b6da127dcda6d13b7dec07dfa2aa21804b553
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-08-27 20:13:45 +0700

    Fixed optimal

--- C_Tutorial/Methods/MaximumMinimum.c
+++ C_Tutorial/Methods/MaximumMinimum.c
@@ -1,27 +1,23 @@
 int max(int num1, int num2)
 {
-    int result;
-    if (num1 > num2)
+    if (num1 >= num2)
     {
-        result = num1;
+        return num1;
     }
     else
     {
-        result = num2;
+        return num2;
     }
-    return result;
 }
 
 int min(int num1, int num2)
 {
-    int result;
-    if (num1 < num2)
+    if (num1 <= num2)
     {
-        result = num1;
+        return num1;
     }
     else
     {
-        result = num2;
+        return num2;
     }
-    return result;
 }
