2984ba7ec9bbf2f84983c94f3bb37b959c6797c7
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-09-03 11:41:19 +0700

    Added more description

--- C_Tutorial/Recursion.c
+++ C_Tutorial/Recursion.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Recursion means the function that call its function itself.
 int BinarySearch(int array[], int low, int high, int find)
 {
     int mid = (high + low) / 2;
@@ -21,12 +22,25 @@ int BinarySearch(int array[], int low, int high, int find)
         }
     }
 }
+// sequence 1 1 2 3 5 8 13 21 ....
+int fibonacci(int n)
+{
+    if (n <= 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
 
 int main()
 {
     int arr[6] = {1, 2, 3, 4, 5, 6};
     int low = 0;
     int high = 6;
-    printf("%d", BinarySearch(arr, low, high, 5));
+    printf("%d\n", BinarySearch(arr, low, high, 5));
+    printf("%d ", fibonacci(5));
     return 0;
 }
