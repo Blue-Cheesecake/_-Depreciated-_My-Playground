111b7c230f0e373c10a1c9ea95edb07d20a9c2db
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-09-10 20:18:35 +0700

    Added more dscription

--- C_Tutorial/EasyLoop.c
+++ C_Tutorial/EasyLoop.c
@@ -22,10 +22,10 @@
     ***o
     ****
 
-    0 1 0 1 0
-    1 0 1 0 1
-    0 1 0 1 0
-    1 0 1 0 1
+    0 A B C D .. N
+    1 0 A B C .. N
+    0 1 0 B C .. N
+    1 0 1 0 C .. N
     .
     .
     N line
@@ -33,70 +33,73 @@
 
 int main()
 {
-    int givenN = 5;
-    int space = 0;
-    for (int i = 0; i < 5; i++)
-    {
-        int count = 0;
-        for (int j = 0; j < space; j++)
-        {
-            printf(" ");
-        }
-        while (count < givenN)
-        {
-            printf("*");
-            count += 1;
-        }
-        givenN -= 1;
-        space += 1;
-        printf("\n");
-    }
+    // int givenN = 5;
+    // int space = 0;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     int count = 0;
+    //     for (int j = 0; j < space; j++)
+    //     {
+    //         printf(" ");
+    //     }
+    //     while (count < givenN)
+    //     {
+    //         printf("*");
+    //         count += 1;
+    //     }
+    //     givenN -= 1;
+    //     space += 1;
+    //     printf("\n");
+    // }
 
-    int half = 5;
-    int expect = 1;
-    int count = 0;
-    while (count < half)
-    {
-        for (int i = 0; i < expect; i++)
-        {
-            printf("*");
-        }
-        expect += 1;
-        count += 1;
-        printf("\n");
-    }
-    while (count > 0)
-    {
-        for (int j = 0; j < count - 1; j++)
-        {
-            printf("*");
-        }
-        count -= 1;
-        printf("\n");
-    }
+    // int half = 5;
+    // int expect = 1;
+    // int count = 0;
+    // while (count < half)
+    // {
+    //     for (int i = 0; i < expect; i++)
+    //     {
+    //         printf("*");
+    //     }
+    //     expect += 1;
+    //     count += 1;
+    //     printf("\n");
+    // }
+    // while (count > 0)
+    // {
+    //     for (int j = 0; j < count - 1; j++)
+    //     {
+    //         printf("*");
+    //     }
+    //     count -= 1;
+    //     printf("\n");
+    // }
 
-    int expecte = 1;
-    int all = 4;
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < expecte; j++)
-        {
-            printf("*");
-        }
-        for (int k = 0; k < all - expecte; k++)
-        {
-            printf("o");
-        }
-        printf("\n");
-        expecte += 1;
-    }
+    // int expecte = 1;
+    // int all = 4;
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     for (int j = 0; j < expecte; j++)
+    //     {
+    //         printf("*");
+    //     }
+    //     for (int k = 0; k < all - expecte; k++)
+    //     {
+    //         printf("o");
+    //     }
+    //     printf("\n");
+    //     expecte += 1;
+    // }
 
     int start = 0;
     // n line
     int totalLine = 4;
-    for (int i = 0; i < 4; i++)
+    int n = start + 1;
+    int c = totalLine - 1;
+    for (int i = 0; i < totalLine; i++)
     {
-        for (int j = 0; j < 5; j++)
+        int A = 65;
+        for (int j = 0; j < n; j++)
         {
             if (start == 0)
             {
@@ -109,7 +112,6 @@ int main()
                 start -= 1;
             }
         }
-        printf("\n");
         if (i % 2 == 0)
         {
             start = 1;
@@ -118,6 +120,14 @@ int main()
         {
             start = 0;
         }
+        for (int ch = 0; ch < c; ch++)
+        {
+            printf("%c ", A);
+            A += 1;
+        }
+        n += 1;
+        c -= 1;
+        printf("\n");
     }
 
     return 0;
