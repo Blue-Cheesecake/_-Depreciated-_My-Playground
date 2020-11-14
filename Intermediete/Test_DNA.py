966dd987645154c3020a000517d429ee6edbeada
SINUT WATTANRPORN <61656182+Blue-Cheesecake@users.noreply.github.com>
2020-11-14 21:27:27 +0700

    String

--- Intermediete/Test_DNA.py
+++ Intermediete/Test_DNA.py
@@ -1,42 +1,34 @@
-# output must be 'ACTGCTA'
-def findDNA(dna1, dna2):
-    if len(dna1) > len(dna2):
-        dna1, dna2 = dna2, dna1
-    room = []
-    for i in range(len(dna1)):
-        for j in range(len(dna1)):
-            if dna1[i] == dna2[j]:
-                state = ''
-                state += dna1[i]
-                k = i + 1
-                l = j + 1
-                while l != len(dna2):
-                    for r, s in zip(range(k, len(dna1)), range(l, len(dna2))):
-                        if dna1[r] == dna2[s]:
-                            state += dna1[r]
-                            k += 1
-                            l += 1
-                        else:
-                            break
-                    break
+class Solution:
+    def longestDNA(self, dna1, dna2):
+        # strict dna1 is the longest DNA
+        if len(dna1) < len(dna2):
+            dna1, dna2 = dna2, dna1
 
-                room.append(state)
-    return room
-
-
-Dna_1 = 'AAAACTGCTACCGGTLMNOPQRST'
-Dna_2 = 'CTGAATCTACTLMNAOPQRSTGCTATTGCAA'
+        result = ''
+        for idx_dna1 in range(len(dna1) - 1):
+            tempResult = ''
+            idx_dna2 = 0
+            while idx_dna2 < len(dna2) - 1:
+                len_to_cmp = 2
+                compareDna1 = dna1[idx_dna1:idx_dna1 + len_to_cmp]
+                compareDna2 = dna2[idx_dna2:idx_dna2 + len_to_cmp]
+                while compareDna1 == compareDna2 and len_to_cmp <= len(dna2):
+                    tempResult = compareDna1
+                    len_to_cmp += 1
+                    if len_to_cmp > len(dna2):
+                        break
+                    compareDna1 = dna1[idx_dna1:idx_dna1 + len_to_cmp]
+                    compareDna2 = dna2[idx_dna2:idx_dna2 + len_to_cmp]
+                    if len(tempResult) > len(result):
+                        result = tempResult
+                idx_dna2 += 1
+        return result
 
 
-rest = findDNA(Dna_1, Dna_2)
-longest = 0
-result = ''
-for i in rest:
-    measure = len(i)
-    if measure > longest:
-        longest = measure
-        result = i
-print(result)
-print(rest)
-
-# solved by Sinut Wattanarporn
+if __name__ == "__main__":
+    s = Solution()
+    print(s.longestDNA(dna1='AAAACTGCTACCGGT', dna2='CTGAATCTACTGCTATTGCAA'))
+    print(s.longestDNA(dna1='AAACTGCACACTGTGTGGGGGACTGGg',
+                       dna2='ACTGTGTGTGACACTGACAGTGACTGGGACTGAAGGGGGGG'))
+    print(s.longestDNA(dna1='AAACTGCACACTGTGTGGGGGACTGGGTGTGGGGGGTAAACCACAACCCC',
+                       dna2='ACTGTGTGTGACACTGACAGTGACTGGGACTGAAGGGGGGGACAAAACCCCAACACACCCCC'))
