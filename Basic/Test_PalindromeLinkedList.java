75bc06f0f24c6e40f028b81dc1b1969cd6a2baa5
Sprite <S_W.piepie@outlook.com.au>
2021-01-26 13:45:34 +0700

    Singly Linked List

--- Basic/Test_PalindromeLinkedList.java
+++ Basic/Test_PalindromeLinkedList.java
@@ -3,23 +3,7 @@
 package Basic;
 
 import java.util.ArrayList;
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
+import Definition.ListNode;
 
 class PalindromLinkedList {
 
---
+++ Definition/ListNode.java
@@ -0,0 +1,18 @@
+package Definition;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
