42f59f6e53cfed185514fc37b1033ca9533636d5
Sprite <S_W.piepie@outlook.com.au>
2021-01-27 21:23:26 +0700

    Binary Tree

--- Definition/TreeNode.java
+++ Definition/TreeNode.java
@@ -5,14 +5,14 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
-    TreeNode() {
+    public TreeNode() {
     }
 
     public TreeNode(int val) {
         this.val = val;
     }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
